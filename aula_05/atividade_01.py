d9786e26ba9a0703cbaf11b1eedc81cbd3b3b521
rafasnunes <rafasnunes@gmail.com>
2025-04-17 19:57:47 -0300

    Início do repositório

---
+++ aula_05/atividade_01.py
@@ -0,0 +1,25 @@
+# Atividade 01
+
+# Crie um sistema "simples" para verificar se a temperatura de um equipamento
+# está dentro de uma faixa segura de operação da máquina.
+
+# A gestora Maria, informou que a temperatura segura é até 15ºC e, que acima
+# disso, o sistema deve informar a manutenção para verificar o sistema de
+# refrigeração.
+
+# O sistema deve permitir que o operador insira a temperatura,
+# no momento da verificação.
+
+# Regras
+
+# Se a temperatura for menor ou igual a 15ºC, informar que o sistema opera
+# dentro da área segura.
+# Do contrário, informar que é necessário manutenção do sistema de refrigeração
+
+temperatura = float(input('Digite a temperatura atual da máquina: '))
+MAX_TEMP = 15
+
+if temperatura <= MAX_TEMP:
+    print('Sistema operando dentro da fiaxa de segurança.')
+else:
+    print('Sistema com problemas! Necessita de manutenção.')
---
+++ aula_05/exemplo_fstring.py
@@ -0,0 +1,10 @@
+# Exemplo 01 - Fstring
+# Mais fácil para imprimir a saída de texto para o usuário.
+
+nota = float(input('Informe a nota: '))
+
+nome = input('Informe o nome do aluno: ')
+
+print(nota)
+
+print(f'A nota de {nome} foi {nota:.2f}')
